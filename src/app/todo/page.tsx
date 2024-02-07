da398a4c0936ce6ed8a0d19254fd87679650a7e5
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2024-02-07 12:32:51 +0900

    ADD:UIの改善

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import React, { useState } from "react";
-import { VStack, Checkbox, Text, StackDivider } from "@chakra-ui/react";
+import {
+  VStack,
+  Input,
+  Checkbox,
+  Text,
+  StackDivider,
+  Box,
+} from "@chakra-ui/react";
 
 import Button from "@/components/button";
-import Input from "@/components/input";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
 
   const addTodo = () => {
     if (inputValue.trim() !== "") {
@@ -29,35 +48,59 @@ const TodoList = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      TODO
-      <VStack spacing={4} align="stretch">
+    <div className="h-screen flex flex-col items-center justify-center text-white">
+      <Text fontSize="24px" fontWeight="bold" textAlign="center" mb={8}>
+        ToDo List
+      </Text>
+      <p className="text-2xl mb-4">
+        Completed Tasks: {count}
+        <Button onClick={reset} style={{ margin: "8px" }}>
+          Reset
+        </Button>
+      </p>
+      <VStack spacing={4} p={4}>
         <Input
-          placeholder="ToDoを追加"
+          placeholder=" Add new todo..."
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          bg="black"
+          h="40px"
         />
-        <Button onClick={addTodo}>追加</Button>
+        <Button onClick={addTodo} style={{ marginTop: "8px" }}>
+          Add
+        </Button>
         <VStack
           divider={<StackDivider />}
           spacing={4}
           align="stretch"
-          maxH="400px"
           overflowY="auto"
         >
           {todos.map((todo, index) => (
-            <Checkbox
+            <Box
               key={index}
-              isChecked={todo.completed}
-              onChange={() => toggleTodo(index)}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
             >
-              <Text textDecoration={todo.completed ? "line-through" : "none"}>
-                {todo.text}
-              </Text>
-              <Button size="xs" onClick={() => deleteTodo(index)}>
-                削除
+              <Checkbox
+                isChecked={todo.completed}
+                onChange={() => toggleTodo(index)}
+              >
+                <Text
+                  textDecoration={todo.completed ? "line-through" : "none"}
+                  onClick={handleIncrement}
+                  onDoubleClick={handleDecrement}
+                >
+                  {todo.text}
+                </Text>
+              </Checkbox>
+              <Button
+                onClick={() => deleteTodo(index)}
+                style={{ margin: "8px" }}
+              >
+                Delete
               </Button>
-            </Checkbox>
+            </Box>
           ))}
         </VStack>
       </VStack>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,8 +6,10 @@ const Button: React.FC<Props> = (props) => {
   return (
     <button
       {...props}
-      className="hover:brightness-110 hover:animate-pulse font-bold py-3 px-6 mb-4 rounded-full bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white"
-    ></button>
+      type="button"
+      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 "
+    >
+    </button>
   );
 };
 
